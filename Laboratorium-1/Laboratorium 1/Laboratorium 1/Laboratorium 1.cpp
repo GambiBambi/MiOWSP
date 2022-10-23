06b64da61b03afc4e1810521e7b6c18411c59016
GambiBambi <102698717+GambiBambi@users.noreply.github.com>
2022-10-23 20:39:38 +0200

    Update Laboratorium 1.cpp

--- Laboratorium-1/Laboratorium 1/Laboratorium 1/Laboratorium 1.cpp
+++ Laboratorium-1/Laboratorium 1/Laboratorium 1/Laboratorium 1.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-float x(int n, float a[], float t[]) {
+float x(int n, vector<float> a, vector<float> t) {
     switch (n) {
     case 0: {
         return 1;
@@ -14,40 +14,42 @@ float x(int n, float a[], float t[]) {
         break;
     }
     case 2: {
-        return ((t[1]*a[1]) + (t[0]*t[0]*a[0]*a[0]))/2;
+        return ((t[1] * a[1]) + (t[0] * t[0] * a[0] * a[0])) / 2;
         break;
     }
     case 3: {
-        return ((t[0]*t[1] + t[1])*a[0]*a[1]) + ((t[0]*t[0]*t[0]*a[0]*a[0]*a[0])/6);
+        return (((t[0] * t[1] * t[1] + t[1]) * a[0] * a[1])/6) + ((t[0] * t[0] * t[0] * a[0] * a[0] * a[0]) / 6);
     }
     default: {
-        return (t[1]*a[1]*x(n-2, a, t)) + (t[0]*a[0]*x(n-1, a, t));
+        return (t[1] * a[1] * x(n - 2, a, t)) + (t[0] * a[0] * x(n - 1, a, t));
         break;
     }
     }
 }
 
-float P(int n, float a[], float t[], int M, int V) {
-    if(n < 0) {
+float P(int n, vector <float> a, vector<float> t, int M, int V) {
+    if (n < 0) {
         return 0;
     }
-    if(n == 0){
-        float t = 0;
+    if (n == 0) {
+        float temp = 0;
         for (int i = 0; i <= V; i++) {
-            t += x(i, a, t);
+            temp += x(i, a, t);
         }
-        return 1/t;
+        return 1 / temp;
     }
-    float Pt = 0;
-    for (int i = 1; i <= M; i++) {
-        Pt+= a[i]*t[i]*P(i - t[i], a, t, M, V);
-    }
-    return Pt/n;
+    float Pt = x(n, a, t) * P(0, a, t, M, V);
+
+    return Pt;
 }
 
-float b(int n, float a[], float t[], int M, int V) {
+float b(int n,  vector<float> a, vector <float> t, int M, int V) {
     if (n >= 0) {
-        return P(n, a, t, M, V) + b(n - 1, a, t, M, V);
+        float Pn = 0;
+        for (int i = n; i <= V; i++) {
+            Pn += P(i, a, t, M, V);
+        }
+        return Pn;
     }
     else {
         return 0;
@@ -59,26 +61,40 @@ int main() {
     int M;
     int V;
     float at, tt;
-    
-    cout << "Podaj wartość M: ";
+
+    cout << "Podaj wartosc M: ";
     cin >> M;
-    cout << "Podaj wartość V: ";
+    cout << "Podaj wartosc V: ";
     cin >> V;
 
     cout << "Podaj parametry a: " << endl;
     for (int i = 0; i < M; i++) {
-        cout << i+1 << ": ";
+        cout << "a" << i + 1 << ": ";
         cin >> at;
         a.push_back(at);
     }
     cout << "Podaj parametry t: " << endl;
     for (int i = 0; i < M; i++) {
-        cout << i+1 << ": ";
-        cin >> t[i];
+        cout << "t" << i + 1 << ": ";
+        cin >> tt;
         t.push_back(tt);
     }
-
-    cout << b(V, a, t, M, V);
+    cout << "Wartosci x: " << endl;
+    for (int i = 0; i <= V; i++) {
+        cout << "x" << i << " = " << x(i, a, t) << endl;
+    }
+    cout << "Wartosci prawdopodobienstwa znalezienia wiazki doskonalej w stanie zajetosci kanalow: " << endl;
+    for (int i = 0; i <= V; i++) {
+        cout << "P" << i << " = " << P(i, a, t, M, V) << endl;
+    }
+    int nActual;
+    int n = 1;
+    nActual = V - t[n-1] + 1;
+    cout << "Prawdopodobienstwo blokady:" << endl;
+    cout << "b(" << n << ") = " << b(nActual, a, t, M, V) << endl;
+    n = 2;
+    nActual = V - t[n-1] + 1;
+    cout << "b(" << n << ") = " << b(nActual, a, t, M, V);
 
 
     return 0;
